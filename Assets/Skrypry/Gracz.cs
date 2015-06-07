c0af84c68cbbee74071a6da2b66597e39c996597
budyn2753 <grzelak.dariusz93@gmail.com>
2015-06-07 23:25:18 +0200

    zmiana
    pierwszy comit

---
+++ Assets/Skrypry/Gracz.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gracz : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		transform.position = new Vector3 (ray.GetPoint (10f).x, 0.0f, transform.position.z);
+	
+	}
+}
